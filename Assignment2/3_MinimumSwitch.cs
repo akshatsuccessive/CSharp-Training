20494010ae697947aec202d589b95ef53698356d
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-10-31 13:17:13 +0530

    Completed Assignment 2

---
+++ Assignment2/3_MinimumSwitch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class MinimumSwitches
+    {
+        private static int CountMinSwitches(int[] bulb, int size)
+        {
+            int count = 0;
+            for(int i = 0; i < size; i++)
+            {
+                if(bulb[i] == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    bulb[i] = 1;
+                    count++;
+                    for(int j = i + 1; j < size; j++)
+                    {
+                        bulb[j] = ~(bulb[j]);
+                    }
+                }
+            }
+            return count;
+        }
+        public static void CountMinimumSwitches()
+        {
+            int size;
+            Console.Write("Enter the size of array : ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            int[] bulb = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(String.Format("Enter the element at {0} index : ", i));
+                bulb[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nThe array is : ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(String.Format("{0} ", bulb[i]));
+            }
+
+            int count = CountMinSwitches(bulb, size);
+            Console.WriteLine(String.Format("\nThe Minimum Number of Switches are : {0}", count));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BalancedIndex.FindBalancedIndex();
+
+            //Leaders.CountLeaders();
+
+            MinimumSwitches.CountMinimumSwitches();
+
+            //MaximumSubarraySum.FindSum();
+        }
+    }
+}
