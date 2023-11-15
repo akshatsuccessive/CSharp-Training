de498a8b8c58dd22e1a3818b55c01774929d9eb8
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-11-15 17:15:57 +0530

    Implementation of Structure using interface

---
+++ Advanced C#/StructureUsingInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_CSharp
+{
+    public struct Books
+    {
+        private string _title;
+        private string _author;
+        private string _subject;
+        private int _bookId;
+
+        public Books(string title, string author, string subject, int bookId)
+        {
+            _title = title;
+            _author = author;
+            _subject = subject;
+            _bookId = bookId;
+        }
+
+        /*
+        public Books()
+        {
+            _author = "Test author";
+            _subject = "Test subject";
+        }
+        */
+
+        public void PrintBooks()
+        {
+            Console.WriteLine("Book Id : {0}, Title : {1}, Author : {2}, Subject : {3}", _bookId, _title, _author, _subject);
+        }
+    }
+    class UseStruct
+    {
+        public static void Method()
+        {
+            Books b1 = new Books("My title", "Akshat", "Test Subject", 7);
+            b1.PrintBooks();
+            Console.ReadKey();
+        }
+    }
+}
