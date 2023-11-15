9b024b3c1c1398b3fc4eb432c213bb9493fcf4f5
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-11-15 17:12:17 +0530

    Generics and Generics Constraints

---
+++ Advanced C#/Generics/GenericConstraints.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_CSharp
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int a, int b)
+        {
+            X = a;
+            Y = b;
+        }
+    }
+    class MyStruct<T> where T : struct                // generic constraints
+    {
+        private T myPoint;
+
+        public MyStruct(T val1)
+        {
+            myPoint = val1;
+        }
+
+        public T GetPoints()
+        {
+            return myPoint;
+        }
+    }
+    class GenericConstraints
+    {
+        public static void Method()
+        {
+            Point P = new Point(10, 20);
+
+            MyStruct<Point> obj = new MyStruct<Point>(P);
+
+            Console.WriteLine("(X, Y) : ({0}, {1})", obj.GetPoints().X, obj.GetPoints().Y);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advanced C#/Generics/Generics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_CSharp
+{
+    class Comparer
+    {
+        // Generic method
+        public bool IsEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+    class TestGenericClass<T>
+    {
+        private T var1;
+        public T var2 { get; set; }
+
+        public TestGenericClass(T value)
+        {
+            var1 = value;
+            var2 = value;
+        }
+
+        public T TestMethod(T param1)
+        {
+            Console.WriteLine("Parameter type: {0}, value {1}", typeof(T).ToString(), param1);
+            Console.WriteLine("Return type: {0}, value {1}", typeof(T).ToString(), var1);
+            return var1;
+        }
+    }
+
+
+    class Generics
+    {
+        public static void Method()
+        {
+            /*
+            Comparer comparer = new Comparer();
+            Console.WriteLine(comparer.IsEqual<int>(8, 9));
+            Console.WriteLine(comparer.IsEqual<string>("xyz", "xyz"));
+            */
+
+            TestGenericClass<int> obj = new TestGenericClass<int>(870);
+            Console.WriteLine(obj.TestMethod(45));
+
+            Console.WriteLine("\n");
+
+            TestGenericClass<string> obj2 = new TestGenericClass<string>("Hello");
+            Console.WriteLine(obj2.TestMethod("Akshat"));
+
+            Console.ReadKey();
+        }
+    }
+}
