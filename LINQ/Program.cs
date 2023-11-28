c128d0d13612ef4ba6cf8adb8099ed67ba5f6dc9
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-11-28 16:25:16 +0530

    Adding menu driven method

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -16,13 +16,74 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            //OrderByThenBy.Method();
-            //Select.Method();
-            //Distinct.Method();
-            //SkipTake.Method();
-            //Where.Method();
-            //UseFirst.Method();
-            UseSingle.Method();
+            bool endProcess = false;
+            do
+            {
+                Console.WriteLine("\nENTER YOUR CHOICE - ");
+                Console.WriteLine("1. Distinct");
+                Console.WriteLine("2. Select");
+                Console.WriteLine("3. Where");
+                Console.WriteLine("4. Skip/Skipwhile and Take/Takewhile");
+                Console.WriteLine("5. First / FirstOrDefault");
+                Console.WriteLine("6. Last / LastOrDefault");
+                Console.WriteLine("7. Single / SingleOrDefault");
+                Console.WriteLine("8. OrderBy and ThenBy");
+                Console.WriteLine("9. Exit");
+
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("\n");
+                        Distinct.Method();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\n");
+                        Select.Method();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\n");
+                        Where.Method();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("\n");
+                        SkipTake.Method();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("\n");
+                        UseFirst.Method();
+                        break;
+
+                    case "6":
+                        Console.WriteLine("\n");
+                        UseLast.Method();
+                        break;
+
+                    case "7":
+                        Console.WriteLine("\n");
+                        UseSingle.Method();
+                        break;
+
+                    case "8":
+                        Console.WriteLine("\n");
+                        OrderByThenBy.Method();
+                        break;
+
+                    case "9":
+                        endProcess = true;
+                        Console.WriteLine("\n\nEXIT.......");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option, Please try again........\n");
+                        break;
+                }
+            }
+            while (!endProcess);
 
             Console.ReadKey();
         }
---
+++ LINQ/UseLast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class UseLast
+    {
+        public static void Method()
+        {
+            List<int> intList = new List<int>() { 10, 65, 3, 88, 52, 22, 90, 11, 4, 56, 20 };
+            Console.WriteLine(intList.Last());
+            Console.WriteLine(intList.LastOrDefault());
+            Console.WriteLine(intList.Last(x => x % 2 != 0));
+            Console.WriteLine(intList.Last(x => x > 80));
+            //Console.WriteLine(intList.Last(x => x == 1));
+            Console.WriteLine(intList.LastOrDefault(x => x == 1));
+        }
+    }
+}
