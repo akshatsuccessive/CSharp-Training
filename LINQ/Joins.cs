a4e7bff74b3d6b823400079ae8516bc516f3779e
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-11-29 17:10:35 +0530

    Set Operations in LINQ

---
+++ LINQ/Joins.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Standard
+    {
+        public int StandardID { get; set; }
+        public string StandardName { get; set; }
+    }
+    class Joins
+    {
+        public static void Method()
+        {
+            List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", StandardID =1 },
+                new Student() { StudentID = 2, StudentName = "Moin", StandardID =1 },
+                new Student() { StudentID = 3, StudentName = "Bill", StandardID =2 },
+                new Student() { StudentID = 4, StudentName = "Ram" , StandardID =2 },
+                new Student() { StudentID = 5, StudentName = "Ron" }
+            };
+
+            List<Standard> standardList = new List<Standard>() {
+                new Standard() { StandardID = 1, StandardName = "Standard 1"},
+                new Standard() { StandardID = 2, StandardName = "Standard 2"},
+                new Standard() { StandardID = 3, StandardName = "Standard 3"}
+            };
+
+
+            var joinResult = studentList.Join(standardList,
+                    student => student.StandardID,
+                    standard => standard.StandardID,
+                    (student, standard) => new
+                    {
+                        StudentName = student.StudentName,
+                        StandardName = standard.StandardName
+                    });
+
+
+            foreach(var item in joinResult)
+            {
+                Console.WriteLine(item.StudentName + ", " + item.StandardName);
+            }
+        }
+    }
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,81 +6,32 @@ using System.Threading.Tasks;
 
 namespace LINQ
 {
-    class Student
+    public class Student
     {
         public int StudentID { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
+        public int StandardID { get; set; }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            bool endProcess = false;
-            do
-            {
-                Console.WriteLine("\nENTER YOUR CHOICE - ");
-                Console.WriteLine("1. Distinct");
-                Console.WriteLine("2. Select");
-                Console.WriteLine("3. Where");
-                Console.WriteLine("4. Skip/Skipwhile and Take/Takewhile");
-                Console.WriteLine("5. First / FirstOrDefault");
-                Console.WriteLine("6. Last / LastOrDefault");
-                Console.WriteLine("7. Single / SingleOrDefault");
-                Console.WriteLine("8. OrderBy and ThenBy");
-                Console.WriteLine("9. Max, Min, Count, Average and Sum");
-                Console.WriteLine("10. Exit");
-
-                string option = Console.ReadLine();
-                switch (option)
-                {
-                    case "1":
-                        Distinct.Method();
-                        break;
-
-                    case "2":
-                        Select.Method();
-                        break;
-
-                    case "3":
-                        Where.Method();
-                        break;
-
-                    case "4":
-                        SkipTake.Method();
-                        break;
-
-                    case "5":
-                        UseFirst.Method();
-                        break;
-
-                    case "6":
-                        UseLast.Method();
-                        break;
-
-                    case "7":
-                        UseSingle.Method();
-                        break;
-
-                    case "8":
-                        OrderByThenBy.Method();
-                        break;
-
-                    case "9":
-                        Aggregate.Method();
-                        break;
-
-                    case "10":
-                        endProcess = true;
-                        Console.WriteLine("\n\nEXIT.......");
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid option, Please try again........\n");
-                        break;
-                }
-            }
-            while (!endProcess);
+            //Where.Method();
+            //SkipTake.Method();
+            //UseFirst.Method();
+            //UseLast.Method();
+            //UseSingle.Method();
+            //OrderByThenBy.Method();
+            //Aggregate.Method();
+            //Joins.Method();
+            //AnyAll.Method();
+            //UseConcat.Method();
+            //Distinct.Method();
+            //Select.Method();
+            //Except.Method();
+            //Intersect.Method();
+            Union.Method();
 
             Console.ReadKey();
         }
