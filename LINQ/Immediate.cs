5a0215abbc12641c1d7f48e2084c21bbc33875a2
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-11-30 17:30:19 +0530

    Deffered execution and Immediate execution

---
+++ LINQ/Immediate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Immediate
+    {
+        private static void PrintStudents(List<Student> studentList)
+        {
+            foreach (var item in studentList)
+            {
+                Console.WriteLine("StudentID : {0}, StudentName : {1}, Age : {2}", item.StudentID, item.StudentName, item.Age);
+            }
+        }
+        public static void Method()
+        {
+            List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 12 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 22 }
+            };
+
+            var tempStudents = studentList.ToList();
+
+            PrintStudents(tempStudents);
+
+            studentList.Add(new Student() { StudentID = 6, StudentName = "Akshat", Age = 21 });
+            studentList.Add(new Student() { StudentID = 7, StudentName = "Bohra", Age = 23 });
+
+            Console.WriteLine("\n-----------------------------\n");
+
+            PrintStudents(tempStudents);
+        }
+    }
+}
